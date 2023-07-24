b241d96d2da656b9d87c3bbb32fdd7b1e751ffab
kimiabeh <110967737+kimiabeh@users.noreply.github.com>
2023-07-25 00:01:17 +0330

    idk:|

--- practice3.js
+++ practice3.js
@@ -1,2 +1,9 @@
-let age = prompt("hii");
-alert("bye" + age);
+// let age = prompt("hii");
+// alert("bye" + age);
+
+function secondIndexOf(s1, s2) {
+  s1 = one;
+  s2 = two;
+  let firstIndex = s1.indexOf(s2);
+  console.log(s1.indexOf(s2, firstIndex + 1));
+}
