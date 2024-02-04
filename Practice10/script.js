3f7f65396573f5b095121581d1ec5f7385b05de5
kimiabeh <110967737+kimiabeh@users.noreply.github.com>
2024-02-04 22:55:37 +0330

    Passing arguments

--- Practice10/script.js
+++ Practice10/script.js
@@ -1,16 +1,45 @@
 // 003 Default Parameters ********
-const bookings = [];
-const createBooking = function (flightNum, numPassenger = 1, price = 200) {
-  const booking = {
-    flightNum,
-    numPassenger,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
-};
-createBooking("LH123");
-createBooking("LH123", 2, 199);
+// const bookings = [];
+// const createBooking = function (flightNum, numPassenger = 1, price = 200) {
+//   const booking = {
+//     flightNum,
+//     numPassenger,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+// createBooking("LH123");
+// createBooking("LH123", 2, 199);
 // 004 How Passing Arguments Works Value vs. Reference ********
 
-// const
+const flight = "LH123";
+const jonas = {
+  name: "jonas",
+  passport: 456784567,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH789";
+  passenger.name = "Mr. " + passenger.name;
+
+  //   if (passenger.passport === 456784567) {
+  //     alert("check in");
+  //   } else {
+  //     alert("wrong passport");
+  //   }
+};
+checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+//is the same as doing
+const flightNum = flight;
+const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+newPassport(jonas);
+checkIn(flight, jonas);
+//005 First-Class and Higher-Order Functions ********
