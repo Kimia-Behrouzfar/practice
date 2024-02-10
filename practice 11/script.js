920f267581739c73a8ac31c65d6abb98aa1ebc36
kimiabeh <110967737+kimiabeh@users.noreply.github.com>
2024-02-10 20:38:22 +0330

    Array Methods

---
+++ practice 11/script.js
@@ -0,0 +1,23 @@
+// 003 Simple Array Methods ****
+let arr = ["a", "b", "c", "d", "e"];
+//SLICE
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(1, -2));
+console.log([...arr]);
+//SPLICE
+//one before and one after!
+// arr.splice(-1);
+// console.log(arr);
+// arr.splice(2, 4);
+// console.log(arr);
+//REVERSE
+// console.log(arr.reverse());
+//CONCAT
+const arr2 = ["j", "i", "h"];
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+//JOIN
+console.log(letters.join("-"));
