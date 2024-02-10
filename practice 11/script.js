28a53dd535cbc26995a2130d7f42cb4232ff06f2
kimiabeh <110967737+kimiabeh@users.noreply.github.com>
2024-02-10 20:52:27 +0330

    Array Methods

--- practice 11/script.js
+++ practice 11/script.js
@@ -1,11 +1,11 @@
 // 003 Simple Array Methods ****
-let arr = ["a", "b", "c", "d", "e"];
+// let arr = ["a", "b", "c", "d", "e"];
 //SLICE
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(1, -2));
-console.log([...arr]);
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(1, -2));
+// console.log([...arr]);
 //SPLICE
 //one before and one after!
 // arr.splice(-1);
@@ -15,9 +15,14 @@ console.log([...arr]);
 //REVERSE
 // console.log(arr.reverse());
 //CONCAT
-const arr2 = ["j", "i", "h"];
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
+// const arr2 = ["j", "i", "h"];
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
 //JOIN
-console.log(letters.join("-"));
+// console.log(letters.join("-"));
+
+//004 The new at Method ****
+const arr = [11, 22, 33];
+console.log(arr[1]);
+console.log(arr.at(0));
